a2d70d211f7be3c66e93803c4e77a6b1951710e8
larryyangsen <larryyangsen@hotmail.com>
2022-10-10 19:29:25 +0800

    update about

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
                 <div className="p-4 col-span-2 text-gray-900 dark:text-white">
                     <div className="text-lg">雲林,台灣</div>
                     <div className="text-lg">楊森豪</div>
-                    <div className="text-lg">全端工程師 React/NodeJS</div>
+                    <div className="text-lg">前端工程師</div>
                     <a
                         className="text-blue-400  dark:text-white hover:text-blue-800 dark:hover:opacity-60"
                         href="mailto:larryyangsen@gmail.com"
@@ -25,7 +25,7 @@ const About = () => {
                             Hi，我是 Larry ，擁有多年前後端經驗，熟悉 Javascript/TypeScript 及 ES6
                             以上語法，前端專注於使用 React 及其生態系包含 React Router、React-Query 、Hooks、Redux
                             等，對前端技術有一定的熟悉與了解。 除了前端技術外，對 Node.JS 使用 Express.JS 搭配 MongoDB
-                            撰寫後端也有一定的經驗，除了前後端外，在 CI/CD 上也有使用過 Drone.IO 工具搭配 Docker
+                            撰寫後端也有一定的經驗，除了前後端外，在 CI/CD 上也有使用過 Drone.IO 與 Gitlab CI 搭配 Docker
                             容器化的來進行自動化部屬的經驗。平常熱衷吸收新的知識，時常看看 Github
                             上面有什麼新的技術，並喜歡自學新的技術。
                         </p>
